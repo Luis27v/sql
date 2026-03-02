11def702d61be5209ccd704a5c6e31c02f7712ef
Luis27v <luiseduardo27v@gmail.com>
2026-03-02 15:10:04 -0300

    Add files via upload
    Aula 02-03-2026

---
+++ 1006.py
@@ -0,0 +1,6 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+MEDIAPOND = (A*2 + B*3 + C*5) / 10
+print (f'MEDIAPOND = {MEDIAPOND: .1f}')
---
+++ 1008.py
@@ -0,0 +1,12 @@
+A = int(input()) #numero do funcionario
+B = int(input()) #numero de horas trabalhadas
+C = float(input()) #valor hora
+
+NUMBER = A
+NUMBER = B
+NUMBER = C
+
+SALARY = (B*C)
+
+print(f'NUMBER = {NUMBER}')
+print(f'U$ SALARY.00 = {SALARY}')

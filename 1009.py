14ab6de2024f34ab404c0fc1b8fa0f4517c765af
Luis27v <luiseduardo27v@gmail.com>
2026-03-02 21:39:45 -0300

    1009.py edit 03-02

---
+++ 1009.py
@@ -0,0 +1,7 @@
+nvendedor = (input())
+salariofixo = float(input())
+totalvendas = float(input())
+
+salariototal = (salariofixo + (totalvendas*0.15))
+
+print (f'salario_total = {salariototal: .2f}')
